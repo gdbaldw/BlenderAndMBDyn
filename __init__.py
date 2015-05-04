03d3bacc62df57da15fd67711c17c4514a7376d3
Doug Baldwin <douglasbaldwin@verizon.net>
2015-05-04 11:18:51 -0400

    Testing __init__

---
+++ __init__.py
@@ -0,0 +1,12 @@
+bl_info = {
+    "name": "MBDyn Modeling and Simulation",
+    "author": "G Douglas Baldwin",
+    "version": (0, 1),
+    "blender": (2, 72, 0),
+    "location": "View3D",
+    "description": "Provides an MBDyn multibody dynamic model design and presentation environment.",
+    "warning": "",
+    "wiki_url": "",
+    "category": "STEM"}
+
+import mbdyn
