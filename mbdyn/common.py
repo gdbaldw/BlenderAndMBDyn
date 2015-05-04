ef5024fbc4434a21fce0818cb8fe193717bac2c1
Doug Baldwin <douglasbaldwin@verizon.net>
2015-05-04 11:10:56 -0400

    Loading initial files

---
+++ mbdyn/common.py
@@ -0,0 +1,178 @@
+# --------------------------------------------------------------------------
+# BlenderAndMBDyn
+# Copyright (C) 2015 G. Douglas Baldwin - http://www.baldwintechnology.com
+# --------------------------------------------------------------------------
+# ***** BEGIN GPL LICENSE BLOCK *****
+#
+#    This file is part of BlenderAndMBDyn.
+#
+#    BlenderAndMBDyn is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Blender MBDyn is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+# ***** END GPL LICENCE BLOCK *****
+# -------------------------------------------------------------------------- 
+
+if "bpy" in locals():
+    import imp
+    imp.reload(bpy)
+    imp.reload(sqrt)
+    imp.reload(bmesh)
+else:
+    import bpy
+    from math import sqrt
+    import bmesh
+
+aerodynamic_types = [
+    "Aerodynamic body",
+    "Aerodynamic beam2",
+    "Aerodynamic beam3",
+    "Generic aerodynamic force"]
+beam_types = [
+    "Beam segment",
+    "Three node beam"]
+force_types = [
+    "Abstract force",
+    "Structural force",
+    "Structural internal force",
+    "Structural couple",
+    "Structural internal couple"]
+genel_types = [
+    "Swashplate"]
+joint_types = [
+    "Axial rotation",
+    "Clamp",
+    "Distance",
+    "Deformable displacement joint",
+    "Deformable hinge",
+    "Deformable joint",
+    "In line",
+    "In plane",
+    "Revolute hinge",
+    "Rod",
+    "Spherical hinge",
+    "Total joint",
+    "Viscous body"]
+environment_types = [
+    "Air properties",
+    "Gravity"]
+node_types = [
+    "Rigid offset",
+    "Dummy node",
+    "Feedback node"]
+
+rigid_body_types = ["Body"]
+
+structural_static_types = aerodynamic_types + joint_types + ["Rotor"] + beam_types + force_types
+
+structural_dynamic_types = rigid_body_types
+
+class Common:
+    def round_vector(self, v):
+        for i in range(3):
+            v[i] = round(v[i], 5)
+        return v
+    def round_matrix(self, r):
+        for i in range(3):
+            r[i] = self.round_vector(r[i])
+        return r
+    def locationVector_write(self, v, text, end=''):
+        v = self.round_vector(v)
+        text.write(str(v[0])+', '+str(v[1])+', '+str(v[2])+end)
+    def rotationMatrix_write(self, rot, text, pad):
+        rot = self.round_matrix(rot)
+        text.write(
+        pad+', '.join([str(rot[0][j]) for j in range(3)])+',\n'+
+        pad+', '.join([str(rot[1][j]) for j in range(3)])+',\n'+
+        pad+', '.join([str(rot[2][j]) for j in range(3)]))
+    def write_node(self, text, i, node=False, position=False, orientation=False, p_label='', o_label=''):
+        rot_i, globalV_i, Node_i = self.rigid_offset(i)
+        localV_i = rot_i*globalV_i
+        rotT = self.objects[i].matrix_world.to_quaternion().to_matrix().transposed()
+        if node:
+            text.write('\t\t'+str(Node_i)+',\n')
+        if position:
+            text.write('\t\t\t')
+            if p_label:
+                text.write(p_label+', ')
+            self.locationVector_write(localV_i, text)
+        if orientation:
+            text.write(',\n\t\t\t')
+            if o_label:
+                text.write(o_label+', ')
+            text.write('matr,\n')
+            self.rotationMatrix_write(rot_i*rotT, text, '\t\t\t\t')
+    def rigid_offset(self, i):
+        if self.objects[i] in self.database.node:
+            ob = self.objects[i]
+        elif self.objects[i] in self.database.rigid_dict:
+            ob = self.database.rigid_dict[self.objects[i]]
+        else:
+            bpy.context.window_manager.popup_menu(lambda self, c: self.layout.label(
+                "Object "+self.objects[i].name+" is not associated with a Node"),
+                title="MBDyn Error", icon='ERROR')
+            print("***Model Error: Object "+self.objects[i].name+" is not associated with a Node")
+            return
+        rot = ob.matrix_world.to_quaternion().to_matrix()
+        globalV = self.objects[i].matrix_world.translation - ob.matrix_world.translation
+        return rot, globalV, self.database.node.index(ob)
+
+def subsurf(obj):
+    if not [m for m in obj.modifiers if m.type == 'SUBSURF']:
+        obj.modifiers.new("Subsurf", 'SUBSURF')
+        obj.modifiers["Subsurf"].levels = 3
+
+def Ellipsoid(obj, mass, mat):
+    if mat.subtype == "eye":
+        diag = [1.]*3
+    else:
+        diag = [mat.floats[4*i] for i in range(3)]
+    s = mat.factor if mat.scale else 1.0
+    s = [0.5*sqrt(x*s/mass) for x in diag]
+    bm = bmesh.new()
+    for v in [(x*s[0],y*s[1],z*s[2]) for z in [-1., 1.] for y in [-1., 1.] for x in [-1., 1.]]:
+        bm.verts.new(v)
+    for f in [(1,0,2,3),(4,5,7,6),(0,1,5,4),(1,3,7,5),(3,2,6,7),(2,0,4,6)]:
+        bm.faces.new([bm.verts[i] for i in f])
+    crease = bm.edges.layers.crease.new()
+    for e in bm.edges:
+        e[crease] = 0.184
+    bm.to_mesh(obj.data)
+    subsurf(obj)
+    bm.free()
+
+def RhombicPyramid(obj):
+    bm = bmesh.new()
+    for v in [(.333,0.,0.),(0.,.666,0.),(-.333,0.,0.),(0.,-.666,0.),(0.,0.,1.)]:
+        bm.verts.new(v)
+    for f in [(3,2,1,0),(0,1,4),(1,2,4),(2,3,4),(3,0,4)]:
+        bm.faces.new([bm.verts[i] for i in f])
+    crease = bm.edges.layers.crease.new()
+    for e in bm.edges:
+        e[crease] = 1.0
+    bm.to_mesh(obj.data)
+    subsurf(obj)
+    bm.free()
+
+def Teardrop(obj):
+    bm = bmesh.new()
+    for v in [(x, y, -.5) for y in [-.5, .5] for x in [-.5, .5]] + [(0.,0.,0.)]:
+        bm.verts.new(v)
+    for q in [(2,3,1,0),(0,1,4),(1,3,4),(3,2,4),(2,0,4)]:
+        bm.faces.new([bm.verts[i] for i in q])
+    crease = bm.edges.layers.crease.new()
+    for i in range(4,8):
+        bm.edges[i][crease] = 1.0
+    bm.to_mesh(obj.data)
+    bm.free()
+    subsurf(obj)
+
---
+++ mbdyn/matrix.py
@@ -0,0 +1,238 @@
+# --------------------------------------------------------------------------
+# BlenderAndMBDyn
+# Copyright (C) 2015 G. Douglas Baldwin - http://www.baldwintechnology.com
+# --------------------------------------------------------------------------
+# ***** BEGIN GPL LICENSE BLOCK *****
+#
+#    This file is part of BlenderAndMBDyn.
+#
+#    BlenderAndMBDyn is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Blender MBDyn is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+# ***** END GPL LICENCE BLOCK *****
+# -------------------------------------------------------------------------- 
+
+if "bpy" in locals():
+    import imp
+    imp.reload(bpy)
+    imp.reload(Operator)
+    imp.reload(Entity)
+else:
+    from mbdyn.base import bpy, Operator, Entity, Props
+
+types = ["3x1", "6x1", "3x3", "6x6", "6xN"]
+
+tree = ["Add Matrix", types]
+
+class Base(Operator):
+    bl_label = "Matrices"
+    bl_options = {'DEFAULT_CLOSED'}
+    @classmethod
+    def make_list(self, ListItem):
+        bpy.types.Scene.matrix_uilist = bpy.props.CollectionProperty(type = ListItem)
+        bpy.types.Scene.matrix_index = bpy.props.IntProperty(default=-1)
+    @classmethod
+    def delete_list(self):
+        del bpy.types.Scene.matrix_uilist
+        del bpy.types.Scene.matrix_index
+    @classmethod
+    def get_uilist(self, context):
+        return context.scene.matrix_index, context.scene.matrix_uilist
+    def set_index(self, context, value):
+        context.scene.matrix_index = value
+
+classes = dict()
+
+for t in types:
+    class DefaultOperator(Base):
+        bl_label = t
+        def defaults(self, context):
+            pass
+        def assign(self, context):
+            self.entity = self.database.matrix[context.scene.matrix_index]
+        def store(self, context):
+            self.entity = self.database.matrix[context.scene.matrix_index]
+        def create_entity(self):
+            return Entity(self.name)
+    classes[t] = DefaultOperator
+
+class MatrixBase(Base):
+    N = None
+    subtype = None
+    floats = bpy.props.CollectionProperty(type = Props.Floats)
+    scale = bpy.props.BoolProperty(name="Scale", description="Scale the vector")
+    factor = bpy.props.FloatProperty(name="Factor", description="Scale factor", default=1.0, min=-9.9e10, max=9.9e10, precision=6)
+    def defaults(self, context):
+        self.floats.clear()
+        for i in range(self.N):
+            self.floats.add()
+        self.entity_name = ""
+    def assign(self, context):
+        self.index = context.scene.matrix_index
+        self.entity = self.database.matrix[self.index]
+        self.entity_name = self.database.matrix[self.index].name
+        self.subtype = self.entity.subtype
+        self.floats.clear()
+        """
+        for value in self.entity.floats:
+            f = self.floats.add()
+            f.value = value
+        """
+        for i in range(self.N):
+            self.floats.add()
+        for i, value in enumerate(self.entity.floats):
+            self.floats[i].value = value
+        self.scale = self.entity.scale
+        self.factor = self.entity.factor
+    def store(self, context):
+        self.entity = self.database.matrix[self.index]
+        self.entity.subtype = self.subtype
+        self.entity.floats = [f.value for f in self.floats]
+        self.entity.scale = self.scale
+        self.entity.factor = self.factor
+    def draw(self, context):
+        self.basis = [self.subtype, self.scale]
+        layout = self.layout
+        layout.label(self.entity_name)
+        layout.prop(self, "subtype")
+        if self.subtype == "matr":
+            row = layout.row()
+            rowrow = row.row()
+            rowrow.enabled = self.scale
+            rowrow.prop(self, "factor")
+            row.prop(self, "scale")
+            for i in range(self.N):
+                layout.prop(self.floats[i], "value", text="x"+str(i+1))
+    def check(self, context):
+        return [None for a, b in zip(self.basis, [self.subtype, self.scale]) if a != b]
+
+class Matrix3x1(Entity):
+    def string(self):
+        if self.subtype == "null":
+            ret = '\n\t\t\tnull'
+        elif self.subtype == "default":
+            ret = '\n\t\t\tdefault'
+        else:
+            ret = '\n\t\t\t'+str(self.floats).strip('[]')
+            if self.scale:
+                ret += ', scale, '+str(self.factor)
+        return ret
+
+class Matrix3x1Operator(MatrixBase):
+    N = 3
+    bl_label = "3x1"
+    subtype = bpy.props.EnumProperty(items=[
+        ("matr", "General", ""),
+        ("null", "Null", ""),
+        ("default", "Default", "")],
+        name="Subtype")
+    def draw(self, context):
+        self.basis = [self.subtype, self.scale]
+        layout = self.layout
+        layout.label(self.entity_name)
+        layout.prop(self, "subtype")
+        if self.subtype not in "null default".split():
+            row = layout.row()
+            rowrow = row.row()
+            rowrow.enabled = self.scale
+            rowrow.prop(self, "factor")
+            row.prop(self, "scale")
+            for i in range(self.N):
+                layout.prop(self.floats[i], "value", text="x"+str(i+1))
+    def create_entity(self):
+        return Matrix3x1(self.name)
+
+classes[Matrix3x1Operator.bl_label] = Matrix3x1Operator
+
+class Matrix6x1(Matrix3x1):
+    pass
+
+class Matrix6x1Operator(Matrix3x1Operator):
+    N = 6
+    bl_label = "6x1"
+    def create_entity(self):
+        return Matrix6x1(self.name)
+
+classes[Matrix6x1Operator.bl_label] = Matrix6x1Operator
+
+class Matrix3x3(Entity):
+    def string(self):
+        if self.subtype == "matr":
+            ret = ('\n\t\t\tmatr,\n'+
+            '\t'*4+str(self.floats[0:3]).strip('[]')+',\n'+
+            '\t'*4+str(self.floats[3:6]).strip('[]')+',\n'+
+            '\t'*4+str(self.floats[6:9]).strip('[]'))
+        elif self.subtype == "sym":
+            ret = ('\n\t\t\tsym,\n'+
+            '\t'*4+str(self.floats[0:3]).strip('[]')+',\n'+
+            '\t'*5+str(self.floats[4:6]).strip('[]')+',\n'+
+            '\t'*6+str(self.floats[8]))
+        elif self.subtype == "skew":
+            ret = '\n\t\t\tskew, '+str([self.floats[i] for i in [7, 2, 3]]).strip('[]')
+        elif self.subtype == "diag":
+            ret = '\n\t\t\tdiag, '+str([self.floats[i] for i in [0, 4, 8]]).strip('[]')
+        elif self.subtype == "eye":
+            ret = '\n\t\t\teye'
+        elif self.subtype == "null":
+            ret = '\n\t\t\tnull'
+        if self.scale:
+            ret += ', scale, '+str(self.factor)
+        return ret
+
+class Matrix3x3Operator(MatrixBase):
+    N = 9
+    bl_label = "3x3"
+    subtype = bpy.props.EnumProperty(items=[
+        ("matr", "General", "General matrix"),
+        ("null", "Null", "Null matrix"),
+        ("sym", "Symmetric", "Symmetrix matrix"),
+        ("skew", "Skew symmetric", "Skew symmetric matrix"),
+        ("diag", "Diagonal", "Diagonal matrix"),
+        ("eye", "Identity", "Identity matrix"),
+        ], name="Subtype", default="eye")
+    def draw(self, context):
+        self.basis = [self.subtype, self.scale]
+        layout = self.layout
+        layout.label(self.entity_name)
+        layout.prop(self, "subtype")
+        if self.subtype != "null":
+            row = layout.row()
+            rowrow = row.row()
+            rowrow.enabled = self.scale
+            rowrow.prop(self, "factor")
+            row.prop(self, "scale")
+            if self.subtype != "eye":
+                for i in range(3):
+                    row = layout.row()
+                    for j in range(3):
+                        k = 3*i+j
+                        if ((self.subtype == "sym" and k in [3,6,7])
+                        or (self.subtype == "skew" and k not in [2,3,7])
+                        or (self.subtype == "diag" and k not in [0,4,8])):
+                            row.label("")
+                        else:
+                            row.prop(self.floats[k], "value", text="x"+str(k+1))
+    def create_entity(self):
+        return Matrix3x3(self.name)
+
+classes[Matrix3x3Operator.bl_label] = Matrix3x3Operator
+
+class Matrix6x6(Entity):
+    ...
+class Matrix6x6Operator(MatrixBase):
+    N = 36
+class Matrix6xN(Entity):
+    ...
+class Matrix6xNOperator(MatrixBase):
+    N = 30
+
